12b7ef8e5f3ce2420402b2d3b6093ab845113a1f
Nick Taras <nicktaras@hotmail.co.uk>
2018-06-05 19:01:24 +1000

    added mock of creedentials file

---
+++ credentials-config-mock.js
@@ -0,0 +1,13 @@
+// Make a copy of this file and name it, credentials-config.js
+// Complete the client_id and Secrets as required.
+
+module.exports = {
+  paypal: {
+    'mode': 'sandbox', // live when public
+    'client_id': '',
+    'client_secret': ''
+  },
+  quickbooks: {},
+  aws: {},
+  google: {}
+}
